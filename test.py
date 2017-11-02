47f9c6a929ab4cdc99d7e004a9016a4b865ac5e8
emingram <eingram16@gmail.com>
2017-11-02 11:47:29 -0500

    Add test file, now it tests the blottogame function with the given match

---
+++ test.py
@@ -0,0 +1,7 @@
+from blottogame import BlottoGame
+
+alice = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]    #testing the function with given match, alice vs. carol
+carol = [15, 5, 30, 5, 10, 5, 0, 10, 0, 20]
+
+print(BlottoGame(alice, carol, 10))
+
