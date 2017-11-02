cbbb6f8491e5cd80f7e30983235a280aab761ebf
emingram <eingram16@gmail.com>
2017-11-02 11:18:10 -0500

    Add function from Wittman paper to start experimenting with

---
+++ stratfunctions.py
@@ -0,0 +1,12 @@
+def RandoStrat (init, n):
+    #create a single random integer-valued strategy that sums to init with length n
+    res = init      #create a temporary resource variable
+    strat = []      #empty list
+    for i in range(n-1):
+        rand = np.random.randint(0, res)    #fill each position in the vector with a random int from 0 to the remaining reserves
+        strat.append(rand)
+        res -= rand         #reduce reserves by this number
+    strat.append(rand)      #add this to the strategy vector
+    random.shuffle(strat)   #shuffle the strategy vector
+    return strat
+
